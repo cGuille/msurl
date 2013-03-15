369d6bd4b809e940a7a8e41248c578f2293b3d5c
Guillaume Charmetant <cguille.dev@gmail.com>
2013-03-15 11:42:16 +0100

    Initial commit

---
+++ msurl.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var config = {
+        'browser-cmd': 'chromium-browser'
+    };
+
+var fs = require('fs'),
+    spawn = require('child_process').spawn;
+
+var urlFilePath = process.argv[2],
+    fileContent,
+    url;
+
+if (!fs.existsSync(urlFilePath)) {
+    fatalError('The given Internet Shortcut file "' + urlFilePath + '" does not exist!');
+}
+
+try {
+    fileContent = fs.readFileSync(urlFilePath).toString();
+} catch (error) {
+    fatalError(error);
+}
+
+var matches = /URL=(.*)/.exec(fileContent);
+if (!matches) {
+    fatalError('The .url file "' + urlFilePath + '" may be malformed')
+}
+
+url = matches[1];
+spawn(config['browser-cmd'], [url]);
+
+function fatalError(message) {
+    console.error(message);
+    process.exit(1);
+}
